9c0d02edc2c5a8c524ace05da7bcdd7bc393ffc5
Peter Siman <peter.siman@wandera.com>
2017-04-11 07:43:51 +0200

    Cookie with username cleared after logout

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -73,7 +73,8 @@ var AuthController = {
     // mark the user as logged out for auth purposes
     req.session.authenticated = false;
 
-    res.redirect('/');
+    res.clearCookie("ws-user");
+    res.send({"success" : "logout-successful"});
   },
 
   /**
